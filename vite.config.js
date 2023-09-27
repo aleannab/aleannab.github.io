53addee0f5e63166bfcc581169b9af6670478354
aleannab <aleannab@gmail.com>
2023-09-27 04:27:34 -0700

    Update config to automate tailwind build command

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import postcss from 'postcss'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), postcss()],
+  build: {
+    rollupOptions: {
+      // Specify entry points for your application here
+      input: {
+        main: './src/main.js', // Adjust the path to your entry file
+      },
+    },
+  },
 })
