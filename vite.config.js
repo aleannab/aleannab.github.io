3b6fd3c35b046fe0fe75ca0c40c18eb09fc6c034
aleannab <aleannab@gmail.com>
2023-10-07 07:17:09 -0700

    Add vite-plugin-copy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { copy } from 'vite-plugin-copy';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: '/',
+  plugins: [react(),
+  copy({
+      targets: [
+        // Copy the specific file(s) you want to preserve in the "dist" directory.
+        { src: 'path/to/output.css', dest: 'dist' },
+        // You can add more copy targets for other files if needed.
+      ],
+      // Adjust other options as needed.
+    }),],
 })
