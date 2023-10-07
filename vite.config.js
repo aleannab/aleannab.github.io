b5b6081bd8c7e277774917bbcda4eb7f5e86db42
aleannab <aleannab@gmail.com>
2023-10-07 06:53:17 -0700

    Update config files

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import postcss from 'postcss'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), postcss()],
-  build: {
-    rollupOptions: {
-      // Specify entry points for your application here
-      input: {
-        main: './src/main.jsx', // Adjust the path to your entry file
-      },
-    },
-  },
+  plugins: [react()],
 })
