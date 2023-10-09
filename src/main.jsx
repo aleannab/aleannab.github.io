fc3b17c733f3089aede1d6cc9fb77a4243d9cb3a
aleannab <aleannab@gmail.com>
2023-10-08 21:53:46 -0700

    Fix scroll reset bug

--- src/main.jsx
+++ src/main.jsx
@@ -1,34 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Root from './routes/Root';
-import ErrorPage from './error-page';
-import Home from './components/Home';
-import ProjectTemplate from './components/ProjectTemplate';
-import ProjectDetails from './components/ProjectDetails';
+import App from './App';
 import './input.css';
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Root />,
-    errorElement: <ErrorPage />,
-    children: [
-      {
-        path: '',
-        element: <Home />,
-      },
-      {
-        path: '/projects/:projectId',
-        element: <ProjectTemplate />,
-        component: { ProjectDetails },
-      },
-    ],
-  },
-]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>
 );
