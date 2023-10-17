f99c3235ea20125db45eb99bfd349a51768ebf99
aleannab <aleannab@gmail.com>
2023-10-17 09:57:42 -0700

    Add background texture

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,6 +7,7 @@ import Footer from './Footer';
 const App = () => {
   return (
     <div className="w-full flex flex-col relative items-center justify-center overflow-hidden">
+      <div className="absolute pattern w-full h-full z-[-30]" />
       <Navigation />
       <Hero />
       <AboutMe />
