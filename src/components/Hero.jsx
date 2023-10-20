1cc6ec134ded05cfb22544c8255d46596cd6d9d5
aleannab <aleannab@gmail.com>
2023-10-19 21:12:08 -0700

    Minor layout changes to hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,12 +11,13 @@ const Hero = () => {
     <div ref={ref} id="home" className="hero relative min-h-screen overflow-y-visible">
       <img src={left} className="absolute top-0 left-0 min-h-full object-cover z-[-20] max-w-2xl" />
       <img src={right} className="absolute top-0 right-0 min-h-full object-cover z-[-30] max-w-2xl" />
-      <div className="hero-content relative text-center">
-        <div className="flex flex-col max-w-6xl reveal-slide-hero opacity-0 gap-5 p-10 rounded-lg bg-base-100/50 md:bg-transparent">
-          <h1 className="reveal-slide-hero text-4xl md:text-5xl lg:text-6xl font-serif font-bold text-primary">Antoinette Bumatay-Chan</h1>
-          <p className="reveal-slide-hero w-auto font-sans">Creative Developer | Art + Technology</p>
-          <p className="reveal-slide-hero w-auto font-sans italic">Still under construction... but feel free to poke around. :]</p>
+      <div className="hero-content flex flex-col relative text-center gap-10">
+        <div className="flex flex-col max-w-6xl reveal-slide-hero opacity-0 gap-2 rounded-lg bg-accent/80 lg:bg-transparent p-10">
+          <h1 className="text-4xl md:text-5xl lg:text-6xl font-serif font-bold text-primary">Antoinette Bumatay-Chan</h1>
+          <h2 className="font-sans text-2xl md:text-3xl drop-shadow-md">Creative Developer | Technology + Art</h2>
         </div>
+        {/* <p className="reveal-fade-hero flex-none font-sans bg-accent text-lg rounded-lg px-2 font-thin">Artistic Byte-sized Concoctions</p> */}
+        <p className="bg-accent/80 text-lg rounded-lg p-2">still under construction... but feel free to poke around. :]</p>
       </div>
     </div>
   );
