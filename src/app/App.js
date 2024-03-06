2b131acca8a9844a4d9d34ed2701401a332712c2
aleannab <aleannab@gmail.com>
2024-03-06 13:28:03 -0800

    Update to use hash router

--- src/app/App.js
+++ src/app/App.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter as Router, useLocation } from 'react-router-dom';
+import { HashRouter as Router, useLocation } from 'react-router-dom';
 import withRouter from '../hooks/withRouter';
 import AppRoutes from './routes';
 import Headermain from '../header';
